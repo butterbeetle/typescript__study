562536b980814d01fa01da55597fa8803f57d448
butterbeetle <butterbeetle@naver.com>
2023-07-03 21:10:27 +0900

    FEAT : TodoComponent, NoteComponent 생성

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -7,7 +7,7 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
       <div class="image__holder">
         <img class="image__thumbnail"></img>
       </div>
-      <p class="image__title"></p>
+      <h2 class="image__title"></h2>
     </section>`);
 
     const imageElement = this.element.querySelector(
